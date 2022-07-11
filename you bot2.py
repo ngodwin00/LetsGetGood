52b5d3c62420b65b723d76376a4aedb46853cb07
ngodwin00 <109047530+ngodwin00@users.noreply.github.com>
2022-07-10 20:06:00 -0400

    Add files via upload

---
+++ you bot2.py
@@ -0,0 +1,18 @@
+name = input('What is your name?')
+yes = ('yes')
+no = ('no')
+name1=(input('Is this correct? , Yes or No?'))
+if name1 == yes:
+    exit(print('Hello', name))
+if name1 == no:
+        print('Ok, Thats weird')
+name2=(input('Did you Forget?'))
+yes2 = ('yes')
+no2 = ('no')
+if name2 == yes2:
+    print('Good luck with that, YOU BOT')
+if name2 == no2:
+    name3 = (input('THEN WHAT THE FUCK IS IT??'))
+    print('JESUS FUCKING CHRIST, HELLO', name3)
+
+
